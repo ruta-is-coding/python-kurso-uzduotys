c1028af9981b09536134a2d50f71d6a4781d8748
Rūta <rutajurg.123@gmail.com>
2024-09-16 19:22:08 +0300

    Homework changes

--- oop-2/uzduotis-2.py
+++ oop-2/uzduotis-2.py
@@ -10,29 +10,27 @@ class Darbuotojas:
         self.valandos_ikainis = valandos_ikainis
         self.dirba_nuo = dirba_nuo
 
-    # Metodas, kuris paskaičiuoja, kiek darbuotojas nudirbo dienų nuo įvestos dienos (dirba_nuo) iki šiandien
+    # Apsaugotas metodas, kuris paskaičiuoja, kiek darbuotojas nudirbo dienų nuo įvestos dienos (dirba_nuo) iki šiandien
     # (turint omeny, kad darbuotojas dirba 7 dienas per savaitę)
-    def kiek_dienu_praejo(self):
+    def _kiek_dienu_nudirbo(self):
         skirtumas = datetime.today() - datetime.strptime(self.dirba_nuo, "%Y-%m-%d")
         return skirtumas.days
 
     # Metodas kuris pasinaudodamas aukščiau aprašytu metodu, paskaičiuoja bendrą atlyginimą
     # (turint omeny, kad darbuotojas dirba 8 valandas per dieną)
     def paskaiciuoti_atlyginima(self):
-        viso_val = self.kiek_dienu_praejo() * 8
+        viso_val = self._kiek_dienu_nudirbo() * 8
         return viso_val * self.valandos_ikainis
 
 # Klasė NormalusDarbuotojas, kuri skaičiuoja atlyginimą turint omeny,
 # kad darbuotojas dirba 5 dienas per savaitę (o ne 7, kaip įprastas darbuotojas).
 class NormalusDarbuotojas(Darbuotojas):
-    def kiek_dienu_praejo(self):
-        dienos = super().kiek_dienu_praejo()
+    def _kiek_dienu_nudirbo(self):
+        dienos = super()._kiek_dienu_nudirbo()
         return math.floor(dienos / 7 * 5)
 
 darbuotojas1 = Darbuotojas("Jonas", 30, "2024-09-01")
-print(f"{darbuotojas1.vardas} iš viso dirbo: {darbuotojas1.kiek_dienu_praejo()} d.")
 print(f"{darbuotojas1.vardas} iš viso uždirbo: {darbuotojas1.paskaiciuoti_atlyginima()} Eur.")
 
 darbuotojas2 = NormalusDarbuotojas("Marytė", 30, "2024-09-01")
-print(f"{darbuotojas2.vardas} iš viso dirbo: {darbuotojas2.kiek_dienu_praejo()} d.")
 print(f"{darbuotojas2.vardas} iš viso uždirbo: {darbuotojas2.paskaiciuoti_atlyginima()} Eur.")
